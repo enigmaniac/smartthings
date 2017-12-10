460504702fc3f056ef416f9db273b0657b2a16fb
Timothy Lovelock <lovelock@gmail.com>
2017-12-10 21:11:41 +0100

    Cleaned up source code

--- devicetypes/enigmaniac/secure-ssr303-boiler-switch.src/secure-ssr303-boiler-switch.groovy
+++ devicetypes/enigmaniac/secure-ssr303-boiler-switch.src/secure-ssr303-boiler-switch.groovy
@@ -24,17 +24,11 @@
 metadata {
 	definition (name: "Secure SSR303 Boiler Switch", namespace: "enigmaniac", author: "Tim Lovelock") {
 		capability "Actuator"
-//		capability "Indicator"
  		capability "Switch"
 		capability "Polling"
 		capability "Refresh"
 		capability "Sensor"
 
-//		fingerprint mfr:"0063", prod:"4952", deviceJoinName: "Z-Wave Wall Switch"
-//		fingerprint mfr:"0063", prod:"5257", deviceJoinName: "Z-Wave Wall Switch"
-//		fingerprint mfr:"0063", prod:"5052", deviceJoinName: "Z-Wave Plug-In Switch"
-//		fingerprint mfr:"0113", prod:"5257", deviceJoinName: "Z-Wave Wall Switch"
-//      fingerprint mfr:"0113", prod:"5257", deviceJoinName: "Z-Wave Wall Switch"
       fingerprint mfr:"0059", prod:"0003", deviceJoinName: "Secure SSR303 Boiler Switch"
 
 	}
@@ -49,11 +43,6 @@ metadata {
 		reply "200100,delay 100,2502": "command: 2503, payload: 00"
 	}
 
-	/*
-    preferences {
-		input "ledIndicator", "enum", title: "LED Indicator", description: "Turn LED indicator... ", required: false, options:["on": "When On", "off": "When Off", "never": "Never"], defaultValue: "off"
-	}
-*/
 	// tile definitions
 	tiles(scale: 2) {
 
@@ -77,14 +66,6 @@ multiAttributeTile(name:"onff", type:"lighting", width: 6, height: 4, canChangeI
 			}
 		}
 */
-/*
-		standardTile("indicator", "device.indicatorStatus", width: 2, height: 2, inactiveLabel: false, decoration: "flat") {
-			state "when off", action:"indicator.indicatorWhenOn", icon:"st.indicators.lit-when-off"
-			state "when on", action:"indicator.indicatorNever", icon:"st.indicators.lit-when-on"
-			state "never", action:"indicator.indicatorWhenOff", icon:"st.indicators.never-lit"
-		}
-        
-        */
 		standardTile("refresh", "device.switch", width: 2, height: 2, inactiveLabel: false, decoration: "flat") {
 			state "default", label:'', action:"refresh.refresh", icon:"st.secondary.refresh"
 		}
@@ -94,25 +75,6 @@ multiAttributeTile(name:"onff", type:"lighting", width: 6, height: 4, canChangeI
 	}
 }
 
-/*
-def updated(){
-  switch (ledIndicator) {
-        case "on":
-            indicatorWhenOn()
-            break
-        case "off":
-            indicatorWhenOff()
-            break
-        case "never":
-            indicatorNever()
-            break
-        default:
-            indicatorWhenOn()
-            break
-    }
-}
-
-*/
 def parse(String description) {
 	def result = null
 	def cmd = zwave.parse(description, [0x20: 1, 0x70: 1])
@@ -190,69 +152,18 @@ def off() {
 		zwave.thermostatModeV2.thermostatModeGet().format()
 	], standardDelay)
     
-    
-    
-    
-def heat() {
-	delayBetween([
-		zwave.thermostatModeV2.thermostatModeSet(mode: 1).format(),
-		zwave.thermostatModeV2.thermostatModeGet().format()
-	], standardDelay)
-
-
-old code:
-		zwave.basicV1.basicSet(value: 0xFF).format(),
-		zwave.switchBinaryV1.switchBinaryGet().format()
-	])
-    
-
-
-
-def off() {
-	delayBetween([
-		zwave.thermostatModeV2.thermostatModeSet(mode: 0).format(),
-		zwave.thermostatModeV2.thermostatModeGet().format()
-	], standardDelay)
-}
 */
 
 def poll() {
 	delayBetween([
 		zwave.switchBinaryV1.switchBinaryGet().format(),
 		zwave.manufacturerSpecificV1.manufacturerSpecificGet().format()
-	])
+	], 100)
 }
 
 def refresh() {
 	delayBetween([
 		zwave.switchBinaryV1.switchBinaryGet().format(),
 		zwave.manufacturerSpecificV1.manufacturerSpecificGet().format()
-	])
+	], 100)
 }
-
-/*
-void indicatorWhenOn() {
-	sendEvent(name: "indicatorStatus", value: "when on", display: false)
-	sendHubCommand(new physicalgraph.device.HubAction(zwave.configurationV1.configurationSet(configurationValue: [1], parameterNumber: 3, size: 1).format()))
-}
-
-void indicatorWhenOff() {
-	sendEvent(name: "indicatorStatus", value: "when off", display: false)
-	sendHubCommand(new physicalgraph.device.HubAction(zwave.configurationV1.configurationSet(configurationValue: [0], parameterNumber: 3, size: 1).format()))
-}
-
-void indicatorNever() {
-	sendEvent(name: "indicatorStatus", value: "never", display: false)
-	sendHubCommand(new physicalgraph.device.HubAction(zwave.configurationV1.configurationSet(configurationValue: [2], parameterNumber: 3, size: 1).format()))
-}
-
-def invertSwitch(invert=true) {
-	if (invert) {
-		zwave.configurationV1.configurationSet(configurationValue: [1], parameterNumber: 4, size: 1).format()
-	}
-	else {
-		zwave.configurationV1.configurationSet(configurationValue: [0], parameterNumber: 4, size: 1).format()
-	}
-}
-
-*/
